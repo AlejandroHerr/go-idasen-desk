48a95018e7fc664609819d8c9c8c81eac29e0ae0
AlejandroHerr <ahernandezc@protonmail.com>
2025-05-10 17:04:41 +0200

    style(scanner): fix lint issues

--- internal/ble/bledev.go
+++ internal/ble/bledev.go
@@ -4,6 +4,6 @@ import (
 	goble "github.com/go-ble/ble"
 )
 
-func NewDevice(_ string, opts ...goble.Option) (d goble.Device, err error) {
+func NewDevice(_ string, opts ...goble.Option) (goble.Device, error) {
 	return DefaultDevice(opts...)
 }
