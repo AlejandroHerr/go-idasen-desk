ecd08ff1d36dbad362d767dd40bc9d0a24254686
AlejandroHerr <ahernandezc@protonmail.com>
2025-05-18 18:04:17 +0200

    chore(gen-auth-tokn): rename cmd

--- cmd/gen-auth-token/main.go
+++ cmd/gen-auth-token/main.go
@@ -11,12 +11,12 @@ import (
 const DefaultTokenLength = 32
 
 func main() {
-	token := fmt.Sprintf("go_idasen_desk_%s", GenerateSecureTokenHex(DefaultTokenLength))
+	token := fmt.Sprintf("go_idasen_desk_%s", GenerateAuthToken(DefaultTokenLength))
 
 	fmt.Fprintf(os.Stdout, "Generated token: %s\n", token)
 }
 
-func GenerateSecureTokenHex(length int) string {
+func GenerateAuthToken(length int) string {
 	// Create a byte slice to store random bytes
 	b := make([]byte, length)
 
